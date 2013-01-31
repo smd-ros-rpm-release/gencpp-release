43f3cd575e83a919b13fbab0dd2d56dac5e18918
Dirk Thomas <dthomas@willowgarage.com>
2013-01-30 17:45:47 -0800

    Rebase from 'upstream' @ version '0.3.4'

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
+import sys
+from xml.etree.ElementTree import ElementTree
 
-d = generate_distutils_setup(
-    packages=['gencpp'],
-    package_dir={'': 'src'},
-    requires=['genmsg']
-)
+try:
+    root = ElementTree(None, 'stack.xml')
+    version = root.findtext('version')
+except Exception, e:
+    print >> sys.stderr, 'Could not extract version from your stack.xml:\n%s' % e
+    sys.exit(-1)
 
-setup(**d)
+setup(name = 'gencpp',
+      version = version,
+      packages = ['gencpp'],
+      package_dir = {'': 'src'},
+      install_requires = ['genmsg'],
+      scripts = ['scripts/gen_cpp.py'],
+      author = "Morgen Kjaergaard, Troy Straszheim, Josh Faust",
+      author_email = "straszheim@willowgarage.com",
+      url = "http://www.ros.org/wiki/gencpp",
+      download_url = "http://pr.willowgarage.com/downloads/gencpp/",
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License" ],
+      description = "ROS msg/srv C++ generation",
+      long_description = """\
+Library and scripts for generating ROS message data structures in C++.
+""",
+      license = "BSD"
+      )
